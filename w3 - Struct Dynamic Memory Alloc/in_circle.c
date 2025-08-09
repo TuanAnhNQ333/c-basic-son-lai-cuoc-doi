03b6ca5e8cc11062d5d4dd9a75a6f4e9addb86a3
AH_NQ <tuananh030305@gmail.com>
2025-08-09 23:11:31 +0700

    check point and circle

--- w3 - Struct Dynamic Memory Alloc/in_circle.c
+++ w3 - Struct Dynamic Memory Alloc/in_circle.c
@@ -17,18 +17,16 @@ typedef struct circle {
 
 int is_in_circle(point_t *p, circle_t *c) { // p : con trỏ đến 1 điểm, c : con trỏ đến 1 đường tròn
     double x_distance, y_distance;
-    x_distance = p->x - c->center.x; // dấu -> là con trỏ trỏ đến struct dùng để truy cập thành viên của struct
-    // p->x là thành viên x của struct point_t mà p trỏ đến
-    // c->center.x : thành viên x của struct point_t mà c->center trỏ đến
-    //  
-    y_distance = p->y - c->center.y;
-    return (x_distance * x_distance + y_distance * y_distance <= c->radius * c->radius);
+    x_distance = p->x - c->center.x;  // khoảng cách p và center theo x
+    y_distance = p->y - c->center.y; // khoảng cách p và center theo y
+    return (x_distance * x_distance + y_distance * y_distance <= c->radius * c->radius); // x^2 + y^2 <= r^2
+    // thoả mãn -> năm trong, còn không thì nằm ngoài
 }
 
 int main() {
     point_t p;
     circle_t c;
-    printf("Enter the point coordinates\n"); scanf("%lf%lf", &p.x, &p.y);
+    printf("Enter the point coordinates\n"); scanf("%lf%lf", &p.x, &p.y); 
     printf("Enter the circle center coordinates\n"); scanf("%lf%lf", &c.center.x, &c.center.y);
     printf("Enter the circle radius\n"); scanf("%lf", &c.radius);
     if(is_in_circle(&p, &c)) {
@@ -38,3 +36,14 @@ int main() {
     }
     return 0;
 }
+/* giải thích về -> và .
+Dấu -> được sử dụng để truy cập thành viên của struct thông qua con trỏ.
+   Ví dụ: p->x có nghĩa là truy cập thành viên x của struct point_t mà p trỏ đến.
+   Tương tự, c->center.x có nghĩa là truy cập thành viên x của struct point_t mà c->center trỏ đến.
+Dấu . được sử dụng để truy cập thành viên của struct thông qua biến struct.
+    Ví dụ: p.x có nghĩa là truy cập thành viên x của struct point_t p.
+    Tương tự, c.center.x có nghĩa là truy cập thành viên x của struct point_t mà c.center trỏ đến.
+    Sự khác biệt chính là dấu -> được sử dụng khi bạn có một con trỏ đến struct,
+         trong khi dấu . được sử dụng khi bạn có một biến struct trực tiếp.
+    
+*/
