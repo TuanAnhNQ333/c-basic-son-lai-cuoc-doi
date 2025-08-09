1ed7dfe46aecbc80e9b08c7d8b3107f57db6823b
AH_NQ <tuananh030305@gmail.com>
2025-08-09 23:04:53 +0700

    check point and circle

--- w3 - Struct Dynamic Memory Alloc/in_circle.c
+++ w3 - Struct Dynamic Memory Alloc/in_circle.c
@@ -15,9 +15,12 @@ typedef struct circle {
     double radius; // bán kính đường tròn
 } circle_t;
 
-int is_in_circle(point_t *p, circle_t *c) {
+int is_in_circle(point_t *p, circle_t *c) { // p : con trỏ đến 1 điểm, c : con trỏ đến 1 đường tròn
     double x_distance, y_distance;
-    x_distance = p->x - c->center.x;
+    x_distance = p->x - c->center.x; // dấu -> là con trỏ trỏ đến struct dùng để truy cập thành viên của struct
+    // p->x là thành viên x của struct point_t mà p trỏ đến
+    // c->center.x : thành viên x của struct point_t mà c->center trỏ đến
+    //  
     y_distance = p->y - c->center.y;
     return (x_distance * x_distance + y_distance * y_distance <= c->radius * c->radius);
 }
@@ -25,7 +28,7 @@ int is_in_circle(point_t *p, circle_t *c) {
 int main() {
     point_t p;
     circle_t c;
-    printf("Enter the poin coordinates\n"); scanf("%lf%lf", &p.x, &p.y);
+    printf("Enter the point coordinates\n"); scanf("%lf%lf", &p.x, &p.y);
     printf("Enter the circle center coordinates\n"); scanf("%lf%lf", &c.center.x, &c.center.y);
     printf("Enter the circle radius\n"); scanf("%lf", &c.radius);
     if(is_in_circle(&p, &c)) {
