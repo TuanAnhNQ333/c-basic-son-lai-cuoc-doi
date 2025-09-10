9d7ce0614185decd75e43b84f0f1ca2b38c73fa8
AH_NQ <tuananh030305@gmail.com>
2025-09-11 00:05:58 +0700

    nothing

---
+++ w4 - Recursion/Greatest-common-divisor.c
@@ -0,0 +1,11 @@
+/*
+BÀI TOÁN ƯỚC SỐ CHUNG LỚN NHẤT (P.02.04.01)
+• Cho hai số nguyên dương a và b. Hãy viết chương trình tìm ước số chung lớn nhất của a và b.
+• Dữ liệu
+• Kết quả
+• Dòng 1: ghi 2 số nguyên dương a và b (1 <= a, b <= 100000)
+• Ghi ra ước số chung lớn nhất của a và b
+stdin
+stdout
+16 24
+8*/
