392b82a0b1be88c12580e7b22c1333e3daae006c
AH_NQ <tuananh030305@gmail.com>
2025-09-11 23:05:38 +0700

    done code

--- w4 - Recursion/Greatest-common-divisor.c
+++ w4 - Recursion/Greatest-common-divisor.c
@@ -8,4 +8,29 @@ BÀI TOÁN ƯỚC SỐ CHUNG LỚN NHẤT (P.02.04.01)
 stdin
 stdout
 16 24
-8*/
+8
+*/
+
+#include <stdio.h>
+
+// Recursive function to find GCD using Euclidean algorithm
+int gcd(int a, int b) {
+    // Base case: if b is 0, a is the GCD
+    if (b == 0) {
+        return a;
+    }
+    // Recursive case: GCD(a,b) = GCD(b, a mod b)
+    return gcd(b, a % b);
+}
+
+int main() {
+    int a, b;
+    
+    // Input two numbers
+    scanf("%d %d", &a, &b);
+    
+    // Calculate and print GCD using recursion
+    printf("%d\n", gcd(a, b));
+    
+    return 0;
+}
