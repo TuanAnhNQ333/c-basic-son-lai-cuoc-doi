2aa7acbcb86b1ca53ff399160f22c2234bbba99d
AH_NQ <tuananh030305@gmail.com>
2025-09-17 11:42:38 +0700

    update case

--- w1 - array string pointer/check_2_same_array.c
+++ w1 - array string pointer/check_2_same_array.c
@@ -1,31 +1,58 @@
+/*
+Mô tả
+Viết một hàm nhận đối số là hai mảng số nguyên, có cùng số phần tử. Hàm trả về:
+1 nếu hai mảng có nội dung trùng nhau 
+Hàm trả về -1 nếu 2 mảng có nội dung đối xứng (mảng này ngược với mảng kia, và hai mảng này không trùng nhau).
+và 0 cho các trường hợp còn lại.
+Kiểm tra hoạt động của hàm qua một chương trình.
+
+int compare_arrays(int arr1[], int arr2[], int size)
+
+Input:
+N
+Các số nguyên của mảng thứ nhất, giữa hai số nguyên ngăn cách nhau bằng dấu cách
+Các số nguyên của mảng thứ hai, giữa hai số nguyên ngăn cách nhau bằng dấu cách
+
+Output:
+Số nguyên mô tả kết quả so sánh hai mảng, mang 1 trong 3 giá trị: 1 -1 0*/
+
 #include <stdio.h>
 
-#define SIZE 5
-int compare_arrays(int arr1[], int arr2[], int size) {
-    int i = 0;
-    for (int i = 0; i < size; i++) {
-        if (arr1[i] != arr2[i]) {
-            return 0; // Mảng khác nhau
+int compare_arrays(int N, int arr1[], int arr2[]) {
+    int result = 0;
+    for (int i = 0; i < N; i++) {
+        if(arr1[i] == arr2[i]) {
+            result = 1;
         }
+        else if(arr1[i] == arr2[N-i-1]) {
+            result = -1; // đói xứng
+        }
+        else   {
+            result = 0; // Mảng khác nhau
+        }    
     }
-    return 1; // Mảng giống nhau
+    return result; // Mảng giống nhau
 
 }
 int main(void) {
-    int input1[SIZE], input2[SIZE];
-    printf("Enter a list of %d integers\n", SIZE);
-    for(int i = 0; i < SIZE; i++) {
+    int N;
+    scanf("%d", &N);
+
+    int input1[N] , input2[N] ;
+    
+    for(int i = 0; i < N; i++) {
         scanf("%d", &input1[i]);
     }
-    printf("Enter a list of %d integers\n", SIZE);
-    for(int i = 0; i < SIZE; i++) {
+    for(int i = 0; i < N; i++) {
         scanf("%d", &input2[i]);
     }
 
-   if (compare_arrays(input1, input2, SIZE)) {
-       printf("Hai mảng giống nhau.\n");
+   if (compare_arrays(N, input1, input2) == 1) {
+       printf("1");
+   } else if(compare_arrays(N, input1, input2) == -1){
+       printf("-1");
    } else {
-       printf("Hai mảng khác nhau.\n");
+        printf("0");
    }
 
    return 0;
