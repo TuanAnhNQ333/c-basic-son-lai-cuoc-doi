0df2edfec0f998dee06af6453630b7e87f074c90
AH_NQ <tuananh030305@gmail.com>
2025-09-25 21:09:29 +0700

    optimize code using 2 pointer with complexity = O(nlogn)

--- HUSTACK/pair-sum-optimize.c
+++ HUSTACK/pair-sum-optimize.c
@@ -21,55 +21,53 @@ PAIR SUM
     7
     5
 */
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-// Hàm so sánh để dùng trong qsort
-int cmp(const void *a, const void *b) {
-    return (*(int*)a - *(int*)b);
+int compare2integer(const void *elementA, const void *elementB) {
+    int valueA = *(const int*)elementA;
+    int valueB = *(const int*)elementB;
+    return valueA - valueB;
 }
-
-// Đếm số cặp có tổng <= x
-long long count_pairs(int arr[], int n, int x) {
-    int left = 0;
-    int right = n - 1;
-    long long count = 0;
-
-    while (left < right) {
-        if (arr[left] + arr[right] <= x) {
-            // Nếu arr[left] + arr[right] <= x
-            // thì tất cả cặp (arr[left], arr[left+1..right]) đều hợp lệ
-            count += (right - left);
-            left++;
+long long countpair(int sortedArray[], int NumberOfElement, int limitX) {
+    long long paircount = 0;
+    int leftIndex = 0;
+    int rightIndex = NumberOfElement - 1;
+    for(int i = 0; i < NumberOfElement; i ++) {
+        if(sortedArray[leftIndex] + sortedArray[rightIndex] <= limitX) {
+            paircount += (rightIndex - leftIndex);
+            leftIndex ++;
         } else {
-            right--;
+            rightIndex --;
         }
     }
-    return count;
+    return paircount;
 }
-
 int main() {
-    int n;
-    scanf("%d", &n);
+    int NumberOfElement;
+    scanf("%d", &NumberOfElement);
 
-    int arr[n];
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+    int ArrayOfElement[NumberOfElement];
+    for(int i = 0; i < NumberOfElement; i ++) {
+        scanf("%d", &ArrayOfElement[i]);
     }
 
-    // Bước 1: Sắp xếp mảng
-    qsort(arr, n, sizeof(int), cmp);
+    qsort(ArrayOfElement, NumberOfElement, sizeof(int), compare2integer);
 
-    int q;
-    scanf("%d", &q);
 
-    // Xử lý từng query
-    while (q--) {
-        int x;
-        scanf("%d", &x);
-        printf("%lld\n", count_pairs(arr, n, x));
+    int numberofquery;
+    scanf("%d", &numberofquery);
+
+    long long result[numberofquery];
+
+    for(int i = 0; i < numberofquery; i ++) {
+        int limitX;
+        scanf("%d", &limitX);
+        
+        result[i] = countpair(ArrayOfElement, NumberOfElement, limitX);
     }
-
+    for(int i = 0; i < numberofquery; i ++) {
+        printf("%lld\n", result[i]);
+    }
     return 0;
 }
-
