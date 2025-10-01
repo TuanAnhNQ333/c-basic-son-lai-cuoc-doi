cd6adfe14411fb45a48ba7f3d6c9cee8d5b6a640
AH_NQ <tuananh030305@gmail.com>
2025-10-01 20:44:56 +0700

    fix

--- HUSTACK/Chapter1/all_divisor.c
+++ HUSTACK/Chapter1/all_divisor.c
@@ -24,7 +24,7 @@ int main() {
             printf("-%d ", i);
         }
     }
-    for(int i = 0; i <= n; i ++) {
+    for(int i = 1; i <= n; i ++) {
         if(n % i == 0) {
             printf("%d ", i);
         }
