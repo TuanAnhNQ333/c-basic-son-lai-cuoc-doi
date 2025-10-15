52724b523ee38463158db7f7fb88f6ea5f79afb3
AH_NQ <tuananh030305@gmail.com>
2025-10-16 00:05:17 +0700

    update

---
+++ HUSTACK/Chapter1/Medium/array_equal.c
@@ -0,0 +1,86 @@
+/*
+Given two sequence a=a
+1,…,an
+ and b=b1,…,bm
+. Check if these sequences are equal.
+Input
+Line 1: contains a positive integer T (1≤T≤10
+2
+) represents number of tests
+Follow are T tests, each of which has the following format
+Line 1: n and m (1≤n,m≤1000)
+Line 2: a
+1
+,…,a
+n
+Line 3: b
+1
+,…,b
+m
+
+
+Output
+Write to stdout T lines, each line t contains 1 if the sequences of the t
+th
+ test are equal, and write 0, otherwise.
+Example
+Input
+3
+3 3
+1 2 3
+1 2 3
+4 2
+1 2 3 4
+1 2
+3 3
+2 4 1
+2 3 1
+Output
+1
+0
+0*/
+#include<stdio.h>
+#include<string.h>
+#define maxlen 10000
+int main() {
+    int array[maxlen] = {0};
+    int savearray1[maxlen] = {0};
+    int savearray2[maxlen] = {0};
+
+    int T ; //test
+    scanf("%d", &T);
+
+    int check[T] ;
+    
+    for(int i = 0; i < T; i ++) {
+        int n, m; // number 
+        scanf("%d %d", &n, &m);
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &array[j]);
+            savearray1[j] = array[j];
+        }
+        for(int k = 0; k < m; k++) {
+            scanf("%d", &array[k]);
+            savearray2[k] = array[k];
+        }
+        if(n != m) {
+            check[i] = 0;
+        } if(n == m) {
+            for(int j = 0; j < n; j ++) {
+                if(savearray1[j] != savearray2[j]) {
+                    check[i] = 0;
+                } else if(savearray1[j] == savearray2[j]){
+                    check[i] = 1;
+                } else {
+                    check[i] = 0;
+                }
+            }
+        }
+    }
+    for(int i = 0; i < T; i ++) {
+         printf("%d\n", check[i]);
+    }
+    
+    return 0;
+
+}
