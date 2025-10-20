a6c2ea56d336bb0ba6171305c92e722320176666
AH_NQ <tuananh030305@gmail.com>
2025-10-21 00:13:18 +0700

    update

--- HUSTACK/Chapter1/Medium/array_equal.c
+++ HUSTACK/Chapter1/Medium/array_equal.c
@@ -58,8 +58,20 @@ int main() {
         for(int i = 0; i < n; i ++) {
             scanf("%d", &a[i]);
         }
-
-        
+        for(int i = 0; i < m; i ++) {
+            scanf("%d", &b[i]);
+        }
+        if(n != m) {
+            check[t] = 0;
+            continue;
+        }
+        check[t] = 1;
+        for(int i = 0; i < n; i ++) {
+            if(a[i] != b[i]) {
+                check[t] = 0;
+                break;
+            }
+        }
     }
     for(int i = 0; i < T; i ++) {
          printf("%d\n", check[i]);
